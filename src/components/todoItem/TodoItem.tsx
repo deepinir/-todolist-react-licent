e87c6c700a63c0b30a925750040fcd733465876d
Pouria Rezaei <pouria.rezaei1992@gmail.com>
2023-02-08 08:48:21 +0330

    add todolist html and css

--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -1,6 +1,51 @@
 import React from "react";
 import styles from "./todoitem.module.css";
+import clsx from "clsx";
 
-export default function TodoItem() {
-  return <div className={styles.container}>todo item</div>;
+interface IProps {
+  id: string | number;
+  text: string;
+  checked: boolean;
+  onEdit?: (id: string | number) => void;
+  onDelete?: (id: string | number) => void;
+  onChangeChecked?: (id: string | number) => void;
+}
+
+export default function TodoItem(props: IProps) {
+  const [showTaskMenu, setShowTaskMenu] = React.useState(false);
+  const { id, text, checked, onEdit, onDelete, onChangeChecked } = props;
+  const settingsElRef = React.useRef<null | HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const listener = (event: any) => {
+      if (!settingsElRef.current?.contains(event.target as Node | null)) {
+        setShowTaskMenu(false);
+      }
+    };
+    document.addEventListener("click", listener);
+    return () => document.removeEventListener("click", listener);
+  }, []);
+
+  return (
+    <li className={styles.task}>
+      <label onClick={() => onChangeChecked?.(id)}>
+        <input type="checkbox" />
+        <p className={clsx(checked && styles.checked)}>{text}</p>
+      </label>
+      <div className={styles.settings} ref={settingsElRef}>
+        <i
+          className="uil uil-ellipsis-h"
+          onClick={() => setShowTaskMenu((prev) => !prev)}
+        ></i>
+        <ul className={clsx(styles["task-menu"], showTaskMenu && styles.show)}>
+          <li onClick={() => onEdit?.(id)}>
+            <i className="uil uil-pen"></i>Edit
+          </li>
+          <li onClick={() => onDelete?.(id)}>
+            <i className="uil uil-trash"></i>Delete
+          </li>
+        </ul>
+      </div>
+    </li>
+  );
 }
--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -4,12 +4,26 @@ import TodoItem from "../todoItem/TodoItem";
 
 export default function Todolist() {
   return (
-    <div className={styles.container}>
-      <input />
-      {[1, 2, 3, 4, 5].map((_, index) => (
-        <TodoItem key={index} />
-      ))}
-      <hr />
+    <div className={styles.wrapper}>
+      <div className={styles["task-input"]}>
+        <img src="bars-icon.svg" alt="icon" />
+        <input type="text" placeholder="Add a new task" />
+      </div>
+      <div className={styles.controls}>
+        <div className={styles.filters}>
+          <span className="active" id="all">
+            All
+          </span>
+          <span id="pending">Pending</span>
+          <span id="completed">Completed</span>
+        </div>
+        <button className={styles["clear-btn"]}>Clear All</button>
+      </div>
+      <ul className={styles["task-box"]}>
+        {[1, 2, 3, 4, 5].map((_, index) => (
+          <TodoItem key={index} id={index} text="name" checked={false} />
+        ))}
+      </ul>
     </div>
   );
 }
